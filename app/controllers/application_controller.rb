040ad6bff34c24bc432a3eb731dac47924aa3bcc
ReixeXD <anderson-silva2003@hotmail.com>
2024-09-03 10:30:33 -0300

    navbar pegando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
 
     include SessionsHelper
+
+    private
+        def require_logged_in_user
+                unless user_signed_in?
+                        flash[:danger]='Area restrita. Realize o login!'
+                        redirect_to entrar_path
+                end
+        end
+    
 end
